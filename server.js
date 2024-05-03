184633f4032d60f0f91bb899eb50c7b0fdd4c309
Peter Muyomba <petermuyomba@Peters-iMac.local>
2024-05-03 09:53:29 -0400

    routes added

--- server.js
+++ server.js
@@ -13,6 +13,77 @@ app.use(express.json());
 app.use(morgan("dev")); //Developer loggers
 
 //Routes...
+//Create route..
+app.post("/products", (req, res) => {
+  const { name, price, quantity, location, active } = req.body;
+
+  if (!name) {
+    return res.status(422).json({ message: "Name input is required." });
+  }
+  const id = crypto.randomUUID();
+
+  products.push({
+    id,
+    name,
+    price,
+    quantity,
+    location,
+    active,
+  });
+  res.status(201).json({ message: "Product created successfully.." });
+});
+
+//Index route...
+app.get("/products", (req, res) => {
+  res.status(200).send(products);
+});
+
+//Show route..
+app.get("/products/:id", (req, res) => {
+  const product = products.find((product) => product.id == req.params.id);
+
+  if (!product) {
+    return res.status(204).json({ message: "No content" });
+  }
+  res.status(200).json(product);
+});
+
+//Update route...
+app.put("/products/:id", (req, res) => {
+  const product = products.find((product) => product.id == req.params.id);
+  if (!product) {
+    return res.status(204).json();
+  }
+  const { name, price, quantity, location, active } = req.body;
+  if (name) {
+    product.name = name;
+  }
+  if (price) {
+    product.price = price;
+  }
+  if (quantity) {
+    product.quantity = quantity;
+  }
+  if (location) {
+    product.location = location;
+  }
+  if ("active" in req.body) {
+    product.active = active;
+  }
+  res.status(200).json({ message: "Product updated successfully" });
+});
+
+//Delete route
+app.delete("/products/:id", (req, res) => {
+  const productIndex = products.findIndex(
+    (product) => product.id == req.params.id
+  );
+  if (productIndex == -1) {
+    res.status(404).json({ message: "No product found" });
+  }
+  products.splice(productIndex, 1);
+  res.status(200).json({ message: "Product successfully deleted.." });
+});
 
 //Port listerner....
 app.listen(port, () => {
